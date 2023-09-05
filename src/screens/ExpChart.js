cfc2dd702473a24d96aafb7d4891784be7435bcb
Solomon141 <31657654+Solomon141@users.noreply.github.com>
2023-09-05 13:01:07 +0000

    Graph width 100 percent

--- src/screens/ExpChart.js
+++ src/screens/ExpChart.js
@@ -6,27 +6,27 @@ import { Chart as ChartJS } from "chart.js/auto";
 function ExpChart({ chartData }) {
     return (
         <>
-        <div className="row">
-            <div className="col-sm-12">
-                Tools i used Daily
+            <div className="row">
+                <div className="col-sm-12">
+                    Tools i used Daily
+                </div>
+                <div>
+                    <Badge bg="secondary">Trello</Badge>
+                    <Badge bg="primary">Github</Badge>
+                    <Badge bg="secondary">Google Drive</Badge>
+                    <Badge bg="primary">Heroku</Badge>
+                </div>
+                <div>
+                    <Badge bg="secondary">Figma</Badge>
+                    <Badge bg="primary">Docker</Badge>
+                    <Badge bg="primary">Typescript</Badge>
+                </div>
             </div>
-           <div>
-           <Badge bg="secondary">Trello</Badge>
-            <Badge bg="primary">Github</Badge>
-            <Badge bg="secondary">Google Drive</Badge>
-            <Badge bg="primary">Heroku</Badge>
-           </div>
-           <div>
-           <Badge bg="secondary">Figma</Badge>
-            <Badge bg="primary">Docker</Badge>
-            <Badge bg="primary">Typescript</Badge>
+            <div className="row">
+                <div className="col-sm-12">
+                    <Bar data={chartData} />
+                </div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col-sm-12">
-                <Bar data={chartData} />
-            </div>
-        </div>
         </>
     );
 }
