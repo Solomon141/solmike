e2a6296e1164b51b63a2a1f192cc4a7f16bf4e51
Solomon141 <weymit2001@gmail.com>
2023-09-15 14:12:45 +0100

    Images of eval

--- src/screens/AboutMe.js
+++ src/screens/AboutMe.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { FaHeart } from "react-icons/fa";
+import Badge from 'react-bootstrap/Badge';
+
 // import profileImage from "./sp.PNG"
 
 function AboutMe() {
   return (
     <div class="section one" id="aboutme" style={{ paddingTop: "8vh" }}>
-      <div class="row" style={{paddingTop:15, paddingBottom:15}}>
+      <div class="row" style={{ paddingTop: 15, paddingBottom: 15 }}>
         <div class="col-sm-4">
           {/* image */}
-          <img style={{width:"100%"}} src="https://avatars.githubusercontent.com/u/31657654?v=4" alt="Profile Image" />
+          <img style={{ width: "100%" }} src="https://avatars.githubusercontent.com/u/31657654?v=4" alt="Profile Image" />
         </div>
         <div class="col-sm-5">
-          <div style={{paddingBottom: 20}}>
+          <div style={{ paddingBottom: 20 }}>
             <h3 style={{ textTransform: 'uppercase' }} class="name">Solomon Gebremichael</h3>
             <h5 class="jobposition"> Senior software developer </h5>
           </div>
-          <p style={{fontSize: 25, display:'flex', columnGap:10}}> 
-          <FaHeart color="orange" /> <FaHeart color="orange" /> <FaHeart color="orange" />
+          <p style={{ fontSize: 25, display: 'flex', columnGap: 10 }}>
+            <FaHeart color="orange" /> <FaHeart color="orange" /> <FaHeart color="orange" />
           </p>
           <p>Javascript, Python</p>
           <p>Nodejs, React, React Native</p>
@@ -24,12 +26,21 @@ function AboutMe() {
         </div>
 
         <div class="col-sm-3">
-          <ul>
-            <li> Codespace </li>
-            <li> Notion </li>
-            <li> Trello </li>
-            <li> Codespace </li>
-          </ul>
+          <div>
+            Tools i used Daily
+          </div>
+          <div>
+            <Badge bg="secondary">Trello</Badge>
+          </div>
+          <div>
+            <Badge bg="primary">Github</Badge>
+          </div>
+          <div>
+            <Badge bg="secondary">Google Drive</Badge>
+          </div>
+          <div>
+            <Badge bg="primary">Github</Badge>
+          </div>
         </div>
       </div>
     </div>
